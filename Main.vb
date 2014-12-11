33e0b4dfb48680fe6e1d77606947ccf878ce59ea
Peter Kowalsky <acheloos@live.at>
2014-12-11 01:41:53 +0100

    First.

---
+++ Main.vb
@@ -0,0 +1,63 @@
+﻿Public Class fuzzyColor
+
+    Function Colo(i As Integer) As Color
+        If i = 0 Then
+            Return Color.Black
+        End If
+        If i = 1 Then
+            Return Color.Red
+        End If
+        If i = 2 Then
+            Return Color.White
+        End If
+        If i = 3 Then
+            Return Color.Green
+        End If
+        If i = 4 Then
+            Return Color.Brown
+        End If
+        If i = 5 Then
+            Return Color.Blue
+        End If
+        If i = 6 Then
+            Return Color.Yellow
+        End If
+        If i = 7 Then
+            Return Color.DarkBlue
+        End If
+        If i = 8 Then
+            Return Color.Cyan
+        End If
+        If i = 9 Then
+            Return Color.Gray
+        End If
+        Return Color.White
+    End Function
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+
+        Dim y As Integer = 0
+        Dim x As Integer = 0
+        Dim z As Integer = Convert.ToInt32(TextBox1.Text) + 1
+        Dim v As Integer = Convert.ToInt32(TextBox2.Text) + 1
+        Dim Img As New Bitmap(v, z)
+
+        If Not TextBox1.Text Is "" Or TextBox2.Text Is "" Or TextBox1.Text Is "0" Or TextBox2.Text Is "0" Then
+            Do
+
+                Img.SetPixel(x, y, Colo(CInt(Int((9 * Rnd())))))
+                y = y + 1
+                If y = Convert.ToInt32(TextBox1.Text) And x < Convert.ToInt32(TextBox2.Text) Then
+                    y = 0
+                    x = x + 1
+                End If
+            Loop Until y = Convert.ToInt32(TextBox1.Text)
+            Img.SetPixel(0, 0, Colo(0))
+            Img.Save("fuzzyColor.JPG", Imaging.ImageFormat.Jpeg)
+        End If
+    End Sub
+
+    Private Sub fuzzyColor_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Resources
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden 
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die mit einer Assembly verknüpft sind.
+
+' Die Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("fuzzyColor")> 
+<Assembly: AssemblyDescription("Random to the roots!")> 
+<Assembly: AssemblyCompany("network-time.org")> 
+<Assembly: AssemblyProduct("fuzzyColor")> 
+<Assembly: AssemblyCopyright("GNU GENERAL PUBLIC LICENSE")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)> 
+
+'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+<Assembly: Guid("12d95577-e171-4c9c-8a59-b93f1ae44df1")> 
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion 
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+' übernehmen, indem Sie "*" eingeben:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("0.0.0.1")> 
+<Assembly: AssemblyFileVersion("0.0.0.1")> 
+
+<Assembly: NeutralResourcesLanguageAttribute("de")> 
